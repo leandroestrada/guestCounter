1f9feb0f147e5c89824fd39b154581131136e8b9
leandroestrada1 <leandro.estrada@itsgroup.com.br>
2020-01-24 16:25:26 -0300

    Adding missing files

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Column(
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      )));
+}
